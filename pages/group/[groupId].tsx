53209521172d55cd9733248297a5ee5bb8c0da79
Jeongjin Oh <ojj991123@gmail.com>
2022-07-25 01:01:32 +0900

    #14 [Feat]: 홈 화면 디자인, 그룹 가입 요청 기능

--- pages/group/[groupId].tsx
+++ pages/group/[groupId].tsx
@@ -1,5 +1,6 @@
 import dayjs from 'dayjs';
 import { ReactElement } from 'react';
+import Avatar from 'react-avatar';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
 import BackButton from '../../components/BackButton';
@@ -10,129 +11,142 @@ import Count from '../../assets/group_count.svg';
 import Period from '../../assets/group_period.svg';
 import Warning from '../../assets/group_warn.svg';
 import useSWR from 'swr';
-import { checkAccessToken } from '../../utils/checkAccessToken';
-
-const profileUrl =
-  'http://www.kpipa.or.kr/upload/book/KP0062/1384146696326_4110.jpg';
+import fetchData from '../../utils/fetchData';
+import { IGroupDetail } from '../../types/IMyGroup';
+import axios from 'axios';
+import { errorTypes } from '../../utils';
 
 const GroupDetail = () => {
-  const { data, error } = useSWR('/api/auth', checkAccessToken);
   const router = useRouter();
   const { query } = router;
+  const { data: groupDetailData } = useSWR<IGroupDetail>(
+    query.groupId && `/api/group/${query.groupId}`,
+    fetchData
+  );
+
+  const onParticipation = () => {
+    const accessToken = localStorage.getItem('accessToken');
+
+    axios
+      .get(`/api/group/${query.groupId}/participation`, {
+        headers: { Authorization: `Bearer ${accessToken}` },
+      })
+      .then((response) => {
+        if (response.status === 200) {
+          alert('해당 그룹에 가입을 요청하였습니다.');
+        }
+      })
+      .catch((error) => {
+        const {
+          data: { errorType },
+        } = error.response;
+        if (errorType === errorTypes.AUTHORIZATION_FAIL) {
+          alert('2개 이상의 그룹에 가입할 수 없습니다.');
+        }
+      });
+  };
+  if (!groupDetailData) {
+    return null;
+  }
+
+  const {
+    name,
+    description,
+    penalty,
+    maxMember,
+    adminId,
+    perWeek,
+    startTime,
+    endTime,
+    tagList,
+    participationMembers,
+  } = groupDetailData;
   return (
     <>
       <div className='bg-white p-6'>
-        <h3 className='mb-4 text-xl font-semibold'>그룹이름글훕일흠</h3>
+        <h3 className='mb-4 text-xl font-semibold'>{name}</h3>
         <ul className='mb-2 flex flex-wrap gap-2'>
-          <li className='rounded-[4px] bg-mint-main px-2 py-1 text-xs'>
-            # {'문학'}
-          </li>
-          <li className='rounded-[4px] bg-mint-main px-2 py-1 text-xs'>
-            # {'문학'}
-          </li>
-          <li className='rounded-[4px] bg-mint-main px-2 py-1 text-xs'>
-            # {'문학'}
-          </li>
+          {tagList.map((tag, index) => {
+            return (
+              <li
+                key={tag}
+                className='rounded-[4px] bg-mint-main px-2 py-1 text-xs'
+              >
+                # {tag}
+              </li>
+            );
+          })}
         </ul>
         <p className='mb-2 rounded-md border border-light-gray py-4 px-[14px] text-sm'>
-          아무말 아무ㅜ뭄말 아ㅏ무무말 얼른 자고 싶다아무말 아무ㅜ뭄말
-          아ㅏ무무말 얼른 자고 싶다
+          {description}
         </p>
         <ul className='flex flex-wrap gap-2'>
           <li className='flex items-center gap-2'>
             <Person />
-            <span className='text-xs text-middle-gray'>4/6명</span>
+            <span className='text-xs text-middle-gray'>{maxMember}명</span>
           </li>
           <li className='flex items-center gap-2'>
             <Count />
-            <span className='text-xs text-middle-gray'>주 {2}회</span>
+            <span className='text-xs text-middle-gray'>주 {perWeek}회</span>
           </li>
           <li className='flex items-center gap-2'>
             <Period />
             <span className='text-xs text-middle-gray'>
-              {dayjs('2022.5.1').format('YYYY.MM.DD')} -{' '}
-              {dayjs('2022.5.1').format('YYYY.MM.DD')}
+              {dayjs(startTime).format('YYYY.MM.DD')} -{' '}
+              {dayjs(endTime).format('YYYY.MM.DD')}
             </span>
           </li>
           <li className='flex items-center gap-2'>
             <Warning />
             <span className='text-xs text-middle-gray'>
-              규칙에 따른 패널티가 존재
+              {penalty ? '규칙에 따른 패널티가 존재' : '패널티 없음'}
             </span>
           </li>
         </ul>
       </div>
       <div className='p-6'>
         <h3 className='mb-4 text-xl font-semibold'>함께 하고 있는 멤버</h3>
-        <ul className='grid  grid-cols-3 gap-3'>
-          <li className='flex flex-col items-center gap-2 rounded-lg border border-light-gray py-4 px-[14px] shadow-lg'>
-            <div className='relative h-[61px] w-[61px]'>
-              <div className='absolute -top-1 right-1/2 z-[1] translate-x-1/2 rounded-full bg-black py-[2px] px-1 text-[8px] text-mint-main'>
-                방장
-              </div>
-              <Image
-                src={profileUrl}
-                alt='프로필'
-                objectFit='cover'
-                layout='fill'
-                className='rounded-full'
-              />
-            </div>
-            <span className='text-sm font-semibold'>프루아</span>
-            <button className='rounded-[4px] bg-mint-main px-4 py-1 text-xs'>
-              필사기록
-            </button>
-          </li>
-          <li className='flex flex-col items-center gap-2 rounded-lg border border-light-gray py-4 px-[14px] shadow-lg'>
-            <div className='relative h-[61px] w-[61px]'>
-              <Image
-                src={profileUrl}
-                alt='프로필'
-                objectFit='cover'
-                layout='fill'
-                className='rounded-full'
-              />
-            </div>
-            <span className='text-sm font-semibold'>프루아</span>
-            <button className='rounded-[4px] bg-mint-main px-4 py-1 text-xs'>
-              필사기록
-            </button>
-          </li>
-          <li className='flex flex-col items-center gap-2 rounded-lg border border-light-gray py-4 px-[14px] shadow-lg'>
-            <div className='relative h-[61px] w-[61px]'>
-              <Image
-                src={profileUrl}
-                alt='프로필'
-                objectFit='cover'
-                layout='fill'
-                className='rounded-full'
-              />
-            </div>
-            <span className='text-sm font-semibold'>프루아</span>
-            <button className='rounded-[4px] bg-mint-main px-4 py-1 text-xs'>
-              필사기록
-            </button>
-          </li>
-          <li className='flex flex-col items-center gap-2 rounded-lg border border-light-gray py-4 px-[14px] shadow-lg'>
-            <div className='relative h-[61px] w-[61px]'>
-              <Image
-                src={profileUrl}
-                alt='프로필'
-                objectFit='cover'
-                layout='fill'
-                className='rounded-full'
-              />
-            </div>
-            <span className='text-sm font-semibold'>프루아</span>
-            <button className='rounded-[4px] bg-mint-main px-4 py-1 text-xs'>
-              필사기록
-            </button>
-          </li>
+        <ul className='grid grid-cols-3 gap-3'>
+          {participationMembers.map((member) => {
+            return (
+              <li
+                key={member.memberId}
+                className='flex flex-col items-center gap-2 rounded-lg border border-light-gray py-4 px-[14px] shadow-lg'
+              >
+                <div className='relative h-[61px] w-[61px]'>
+                  {member.memberId === adminId && (
+                    <div className='absolute -top-2 right-1/2 z-[1] translate-x-1/2 rounded-full bg-black py-[2px] px-2 text-[8px] text-mint-main'>
+                      방장
+                    </div>
+                  )}
+                  {member.image ? (
+                    <Image
+                      src={member.image}
+                      alt='프로필'
+                      objectFit='cover'
+                      layout='fill'
+                      className='rounded-full'
+                    />
+                  ) : (
+                    <Avatar name={member.nickname} size='61' round={true} />
+                  )}
+                </div>
+                <span className='text-sm font-semibold'>{member.nickname}</span>
+                <button className='rounded-[4px] bg-mint-main px-4 py-1 text-xs'>
+                  필사기록
+                </button>
+              </li>
+            );
+          })}
         </ul>
       </div>
-      <span className='sticky inset-x-0 bottom-0 flex justify-center bg-black py-4 text-xl font-bold text-mint-main'>
+      <button
+        type='button'
+        onClick={onParticipation}
+        className='fixed inset-x-0 bottom-0 flex justify-center bg-black py-4 text-xl font-bold text-mint-main'
+      >
         그룹 신청하기
-      </span>
+      </button>
     </>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,37 +6,58 @@ import GroupItem from '../components/GroupItem';
 import Header from '../components/header';
 import HomeLayout from '../components/home-layout';
 import { IGroup } from '../types/IGroup';
-import { IMyGroup } from '../types/IMyGroup';
+import { IGroupDetail } from '../types/IMyGroup';
 import fetchData from '../utils/fetchData';
 import Period from '../assets/group_period.svg';
+import Avatar from 'react-avatar';
 
-const myGroupData: IMyGroup = {
-  groupId: 10,
-  name: '오정진바보',
-  description: '바보오정진놀리는그룹입니다 수공',
-  penalty: true,
-  maxMember: 5,
-  adminId: 2,
-  perWeek: 5,
-  startTime: '2022-08-30T15:00:00',
-  endTime: '2022-09-01T15:00:00',
-  tagList: ['정진아', '밥은먹고', '개발하니'],
-  participationMembers: [
-    {
-      memberId: 2,
-      nickname: '테스터둘',
-      image: null,
-    },
-  ],
-  createdAt: '2022-07-24T17:10:58.500799',
-  updatedAt: '2022-07-24T17:10:58.500799',
-};
+// const myGroupData: IGroupDetail = {
+//   groupId: 10,
+//   name: '오정진바보',
+//   description: '바보오정진놀리는그룹입니다 수공',
+//   penalty: true,
+//   maxMember: 5,
+//   adminId: 2,
+//   perWeek: 5,
+//   startTime: '2022-08-30T15:00:00',
+//   endTime: '2022-09-01T15:00:00',
+//   tagList: ['정진아', '밥은먹고', '개발하니'],
+//   participationMembers: [
+//     {
+//       memberId: 2,
+//       nickname: '테스터둘',
+//       image: null,
+//     },
+//     {
+//       memberId: 3,
+//       nickname: '테스터둘',
+//       image: 'http://www.kpipa.or.kr/upload/book/KP0062/1384146725763_4162.jpg',
+//     },
+//     {
+//       memberId: 4,
+//       nickname: '테스터둘',
+//       image: 'http://www.kpipa.or.kr/upload/book/KP0062/1384146725763_4162.jpg',
+//     },
+//     {
+//       memberId: 5,
+//       nickname: '테스터둘',
+//       image: null,
+//     },
+//     {
+//       memberId: 6,
+//       nickname: '테스터둘',
+//       image: 'http://www.kpipa.or.kr/upload/book/KP0062/1384146725763_4162.jpg',
+//     },
+//   ],
+//   createdAt: '2022-07-24T17:10:58.500799',
+//   updatedAt: '2022-07-24T17:10:58.500799',
+// };
 const Home = () => {
   const { data: groupData } = useSWR<IGroup[]>(
     '/api/group/recommendation',
     fetchData
   );
-  // const { data: myGroupData } = useSWR<IMyGroup>('/api/group', fetchData);
+  const { data: myGroupData } = useSWR<IGroupDetail>('/api/group', fetchData);
 
   return (
     <div className='h-screen bg-black'>
@@ -51,7 +72,7 @@ const Home = () => {
         </h1>
         <div className='text-xl'>
           <p>
-            안녕하세요 <span className='text-mint-main'>USER NAME</span>님,
+            안녕하세요 <span className='text-mint-main'>알로하</span>님,
           </p>
           <p>오늘도 필사해볼까요?</p>
         </div>
@@ -70,44 +91,70 @@ const Home = () => {
             <span className='text-xs text-middle-gray'>이동진</span>
           </div>
         </div>
-
-        <div className='mb-4'>
-          <h3 className='mb-2 text-xl font-semibold'>추천그룹</h3>
-          <ul className='grid grid-cols-2 gap-3'>
-            {groupData &&
-              (groupData.length !== 0 ? (
-                groupData.map((group) => {
-                  return <GroupItem key={group.groupId} group={group} />;
-                })
-              ) : (
-                <p>추천 그룹 없음</p>
-              ))}
-          </ul>
-        </div>
+        {myGroupData === null && (
+          <div className='mb-4'>
+            <h3 className='mb-2 text-xl font-semibold'>추천그룹</h3>
+            <ul className='grid grid-cols-1 gap-3 sm:grid-cols-2'>
+              {groupData &&
+                (groupData.length !== 0 ? (
+                  groupData.map((group) => {
+                    return <GroupItem key={group.groupId} group={group} />;
+                  })
+                ) : (
+                  <p>추천 그룹 없음</p>
+                ))}
+            </ul>
+          </div>
+        )}
 
         {myGroupData && (
-          <div>
-            <h3 className='mb-2 text-xl font-semibold'>내 그룹</h3>
+          <dl>
+            <dt className='mb-2 text-xl font-semibold'>내 그룹</dt>
             <div className='flex flex-col gap-4 rounded-lg border p-4'>
               <div className='flex'>
                 <span className='flex-1 text-xl font-semibold'>
                   {myGroupData.name}
                 </span>
-                <ul>이미지들</ul>
+                <dd className='flex justify-end -space-x-3'>
+                  {myGroupData.participationMembers.map((member) => {
+                    return (
+                      <div
+                        key={member.memberId}
+                        className='relative h-8 w-8 rounded-full bg-light-gray'
+                      >
+                        {member.image === null ? (
+                          <Avatar
+                            name={member.nickname}
+                            size='32'
+                            round={true}
+                          />
+                        ) : (
+                          <Image
+                            src={member.image}
+                            alt='member'
+                            objectFit='cover'
+                            layout='fill'
+                            className='rounded-full'
+                          />
+                        )}
+                      </div>
+                    );
+                  })}
+                </dd>
               </div>
-              <div className='flex items-center gap-1'>
+              <dd className='flex items-center gap-1'>
                 <Period />
                 <span className='text-middle-gray'>
                   {dayjs(myGroupData.startTime).format('YY.MM.DD')}-
                   {dayjs(myGroupData.endTime).format('YY.MM.DD')}
                 </span>
-              </div>
-              <div className='flex items-center gap-1'>
+              </dd>
+              <dd className='flex items-center gap-1'>
                 <Period />
                 <span className='text-middle-gray'>
                   일주일 {myGroupData.perWeek}개 업로드
                 </span>
-              </div>
+              </dd>
               <button
                 type='button'
                 className='flex w-full justify-center rounded-lg bg-black py-2 font-semibold text-mint-main'
@@ -115,7 +162,7 @@ const Home = () => {
                 + 필사 올리기
               </button>
             </div>
-          </div>
+          </dl>
         )}
       </div>
     </div>
