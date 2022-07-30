c544f33a74443a7ca143f3351c35ca7c87a54497
Jeongjin Oh <ojj991123@gmail.com>
2022-07-30 20:57:59 +0900

    [Fix]: 홈화면 내 그룹 버튼 수정

--- pages/index.tsx
+++ pages/index.tsx
@@ -92,47 +92,49 @@ const Home = () => {
           {myGroupData && (
             <dl>
               <dt className='mb-2 text-xl font-semibold'>내 그룹</dt>
-              <div className='flex flex-col gap-4 rounded-lg border p-4'>
-                <div className='flex'>
-                  <span className='flex-1 text-xl font-semibold'>
-                    {myGroupData.name}
-                  </span>
-                  <dd className='flex justify-end -space-x-3'>
-                    {myGroupData.participationMembers
-                      .slice(0, 4)
-                      .map((member) => {
-                        return (
-                          <CustomAvatar
-                            key={member.memberId}
-                            image={member.image}
-                            nickname={member.nickname}
-                            width={'w-8'}
-                            height={'h-8'}
-                            size={'32'}
-                          />
-                        );
-                      })}
-                  </dd>
-                </div>
-                <dd className='flex items-center gap-1'>
-                  <Period width={24} height={24} />
-                  <span className='text-middle-gray'>
-                    {dayjs(myGroupData.startTime).format('YYYY.MM.DD')} ~{' '}
-                    {dayjs(myGroupData.endTime).format('YYYY.MM.DD')}
-                  </span>
-                </dd>
-                <dd className='flex items-center gap-1'>
-                  <Notice width={24} height={24} />
-                  <span className='text-middle-gray'>
-                    일주일 {myGroupData.perWeek}개 업로드
-                  </span>
-                </dd>
-                <Link href={`/mygroup/${myGroupData.groupId}`}>
-                  <a className='flex w-full justify-center rounded-lg bg-black py-2 font-semibold text-mint-main'>
-                    + 필사 올리기
-                  </a>
-                </Link>
-              </div>
+              <Link href={`/mygroup/${myGroupData.groupId}`}>
+                <a>
+                  <div className='flex flex-col gap-4 rounded-lg border p-4'>
+                    <div className='flex'>
+                      <span className='flex-1 text-xl font-semibold'>
+                        {myGroupData.name}
+                      </span>
+                      <dd className='flex justify-end -space-x-3'>
+                        {myGroupData.participationMembers
+                          .slice(0, 4)
+                          .map((member) => {
+                            return (
+                              <CustomAvatar
+                                key={member.memberId}
+                                image={member.image}
+                                nickname={member.nickname}
+                                width={'w-8'}
+                                height={'h-8'}
+                                size={'32'}
+                              />
+                            );
+                          })}
+                      </dd>
+                    </div>
+                    <dd className='flex items-center gap-1'>
+                      <Period width={24} height={24} />
+                      <span className='text-middle-gray'>
+                        {dayjs(myGroupData.startTime).format('YYYY.MM.DD')} ~{' '}
+                        {dayjs(myGroupData.endTime).format('YYYY.MM.DD')}
+                      </span>
+                    </dd>
+                    <dd className='flex items-center gap-1'>
+                      <Notice width={24} height={24} />
+                      <span className='text-middle-gray'>
+                        일주일 {myGroupData.perWeek}개 업로드
+                      </span>
+                    </dd>
+                    <button className='flex w-full justify-center rounded-lg bg-black py-2 font-semibold text-mint-main'>
+                      + 필사 올리기
+                    </button>
+                  </div>
+                </a>
+              </Link>
             </dl>
           )}
         </div>
